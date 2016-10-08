963035685be0245e21faae227bd515a31b599f8a
Doc999tor <doc999tor@gmail.com>
2016-10-08 05:02:55 +0300

    Added: dynamic build of grid, added a button for adding new row

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ExcelToHtmlGrid
 Takes copied cells from Excel file and pastes it into HTML inputs grid
+
+### How to use it:
--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-Array.from(document.querySelectorAll('input')).forEach(input => {
-	input.addEventListener('paste', e => {
-		e.preventDefault();
-		e.stopPropagation();
-		addData(e.currentTarget, e.clipboardData.getData('Text'));
-		return false;
+window.addEventListener('load', e => {
+	buildGrid();
+
+	Array.from(document.querySelectorAll('input')).forEach(input => {
+		input.addEventListener('paste', e => {
+			e.preventDefault();
+			e.stopPropagation();
+			addData(e.currentTarget, e.clipboardData.getData('Text'));
+			return false;
+		});
 	});
-});
+})
+
 
 function addData (input, text) {
 	console.time('addData');
 	let [rowInput, columnInput] = [
-		parseInt(input.dataset.row.match(/\d/)[0]),
-		parseInt(input.dataset.column.match(/\d/)[0])
+		parseInt(input.dataset.row),
+		parseInt(input.dataset.column)
 	];
 
 	let cells = [];
@@ -24,7 +29,7 @@ function addData (input, text) {
 
 	cells.forEach((rowData, indexRow) => {
 		rowData.forEach((value, indexColumn) => {
-			let query = `input[data-row=row-${indexRow + rowInput}][data-column=cl-${indexColumn + columnInput}]`;
+			let query = `input[data-row="${indexRow + rowInput}"][data-column="${indexColumn + columnInput}"]`;
 			let el = document.querySelector(query);
 			if (el) {
 				el.value = value;
@@ -32,4 +37,28 @@ function addData (input, text) {
 		});
 	});
 	console.timeEnd('addData');
+}
+
+function buildGrid () {
+	const rows = 3;
+	console.time('building');
+	for (let i = 0; i < rows; i++) {
+		buildRow(i);
+	}
+	console.timeEnd('building');
+}
+
+function buildRow (i) {
+	let clone = document.importNode(document.getElementById('template_row').content, true);
+	clone.querySelector('section').dataset.row = i;
+	Array.from(clone.querySelectorAll('section input')).forEach(input => {
+		input.dataset.row = i;
+		input.value = 'row-' + i + ' ' + input.value;
+	});
+	document.querySelector('.container').appendChild(clone);
+}
+
+function addRow() {
+	let lastRow = parseInt(Array.from(document.querySelectorAll('.container section')).pop().dataset.row);
+	buildRow(lastRow+1);
 }
