ce0648cec4ceb7bb9f1500387ee1adb02e423210
Victor <doc99tor@gmail.com>
2017-06-04 02:53:24 +0300

    added backslashes for quotes in output sql string

--- server.php
+++ server.php
@@ -1,7 +1,5 @@
 <?php
 
-// echo '<pre>' . var_export($_POST, true) . '</pre>';
-
 $post = array_slice($_POST, 0);
 
 # Helpers
@@ -19,25 +17,38 @@ for ($i=0; $i < count($hidden_input_names); $i++) {
 
 $row_length = count($post) / $rows_num;
 
-$categories = array_map(function ($item) use ($separator) {
-	return explode($separator, $item)[0];
-}, array_keys(array_slice($post, 0, $row_length)));
+# Categories
+$categories = get_categories($post, $row_length, $separator);
 
-foreach ($post as $key => $value) {
-	if ((string)((float)($value)) != (string)$value) {
-		$post[$key] = '\'' . $value . '\'';
-	}
-}
-
-$values = [];
-for ($i=0; $i < $rows_num; $i++) {
-	$values []= '(' . implode(',', array_slice($post, $i*$row_length, $row_length)) . ')';
-	if ($values[$i] === '(' . implode(',', array_fill(0, $row_length, '\'\'')) . ')') {
-		unset($values[$i]);
-	}
-}
-if (count($values) === 0) {throw new Exception('there is no data', 1);
-}
+# Values
+$values = get_values($post, $rows_num, $row_length);
 
 $query = 'INSERT into mytable (' . implode(',', $categories) . ') values ' . implode(', ', $values) . ';';
 echo $query . "<br>";
+
+function get_categories($post, $row_length, $separator) {
+	return array_map(function ($item) use ($separator) {
+		return explode($separator, $item)[0];
+	}, array_keys(array_slice($post, 0, $row_length)));
+}
+
+function get_values($post, $rows_num, $row_length) {
+	foreach ($post as $key => $value) {
+		if ((string)((float)($value)) != (string)$value) {
+			$post[$key] = '\'' . addslashes($value) . '\'';
+		}
+	}
+
+	$values = [];
+	for ($i=0; $i < $rows_num; $i++) {
+		$values []= '(' . implode(',', array_slice($post, $i*$row_length, $row_length)) . ')';
+		if ($values[$i] === '(' . implode(',', array_fill(0, $row_length, '\'\'')) . ')') {
+			unset($values[$i]);
+		}
+	}
+	if (!count($values)) {
+		throw new Exception('there is no data', 1);
+	}
+
+	return $values;
+}
